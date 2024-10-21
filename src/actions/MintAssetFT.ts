c11ed959a92e26f0fccf66d89011b8f719710b64
developerayo <shodipovi@gmail.com>
2024-10-21 11:28:11 +0100

    ref: update `MintAssetFT` to use string instead of bytes

--- src/actions/MintAssetFT.ts
+++ src/actions/MintAssetFT.ts
@@ -1,79 +1,90 @@
 // Copyright (C) 2024, Nuklai. All rights reserved.
 // See the file LICENSE for licensing terms.
 
-import { Id } from '@avalabs/avalanchejs'
-import {actions, codec, consts, utils} from '@nuklai/hyperchain-sdk'
+import { Address } from "@avalabs/avalanchego-utils";
+import { Action, Codec } from "@nuklai/nuklai-sdk";
 import {
-    MINTASSET_COMPUTE_UNITS,
-    MINTASSET_FT_ID,
-    STORAGE_ASSET_CHUNKS,
-    STORAGE_BALANCE_CHUNKS
-} from '../constants'
+  MINTASSET_FT_COMPUTE_UNITS,
+  MINTASSET_FT_ID,
+  STORAGE_ASSET_CHUNKS,
+  STORAGE_BALANCE_CHUNKS,
+} from "../constants";
 
-export const MintAssetFTTxSize =
-    consts.ADDRESS_LEN + consts.ID_LEN + consts.UINT64_LEN
+export class MintAssetFT implements Action {
+  public assetAddress: Address;
+  public value: bigint;
+  public to: Address;
 
-export class MintAssetFT implements actions.Action {
-    public to: utils.Address
-    public asset: Id
-    public value: bigint
+  constructor(assetAddress: string, value: bigint, to: string) {
+    this.assetAddress = Address.fromString(assetAddress);
+    this.value = value;
+    this.to = Address.fromString(to);
+  }
 
-    constructor(to: string, asset: string, value: bigint) {
-        this.to = utils.Address.fromString(to)
-        this.asset = utils.toAssetID(asset)
-        this.value = value
-    }
+  getTypeId(): number {
+    return MINTASSET_FT_ID;
+  }
 
-    getTypeId(): number {
-        return MINTASSET_FT_ID
-    }
+  computeUnits(): number {
+    return MINTASSET_FT_COMPUTE_UNITS;
+  }
 
-    size(): number {
-        return MintAssetFTTxSize
-    }
+  static unmarshal(codec: Codec): MintAssetFT {
+    const assetAddress = codec.unpackAddress();
+    const value = codec.unpackUint64(false);
+    const to = codec.unpackAddress();
+    return new MintAssetFT(assetAddress.toString(), value, to.toString());
+  }
 
-    computeUnits(): number {
-        return MINTASSET_COMPUTE_UNITS
-    }
+  toJSON(): object {
+    return {
+      assetAddress: this.assetAddress.toString(),
+      value: this.value.toString(),
+      to: this.to.toString(),
+    };
+  }
 
-    stateKeysMaxChunks(): number[] {
-        return [STORAGE_ASSET_CHUNKS, STORAGE_BALANCE_CHUNKS]
-    }
+  toString(): string {
+    return JSON.stringify(this.toJSON());
+  }
 
-    toJSON(): object {
-        return {
-            to: this.to.toString(),
-            asset: this.asset.toString(),
-            value: this.value.toString()
-        }
-    }
+  stateKeysMaxChunks(): number[] {
+    return [STORAGE_ASSET_CHUNKS, STORAGE_BALANCE_CHUNKS];
+  }
+}
 
-    toString(): string {
-        return JSON.stringify(this.toJSON())
-    }
+export class MintAssetFTResult {
+  public oldBalance: bigint;
+  public newBalance: bigint;
 
-    toBytes(): Uint8Array {
-        const codec = utils.Codec.newWriter(this.size(), this.size())
-        codec.packAddress(this.to)
-        codec.packID(this.asset)
-        codec.packUint64(this.value)
-        return codec.toBytes()
-    }
+  constructor(oldBalance: bigint, newBalance: bigint) {
+    this.oldBalance = oldBalance;
+    this.newBalance = newBalance;
+  }
 
-    static fromBytes(bytes: Uint8Array): [MintAssetFT, Error?] {
-        const codec = utils.Codec.newReader(bytes, bytes.length)
-        const to = codec.unpackAddress()
-        const asset = codec.unpackID(false)
-        const value = codec.unpackUint64(true)
-        const action = new MintAssetFT(to.toString(), asset.toString(), value)
-        return [action, codec.getError()]
-    }
+  getTypeId(): number {
+    return MINTASSET_FT_ID;
+  }
 
-    static fromBytesCodec(codec: utils.Codec): [MintAssetFT, utils.Codec] {
-        const to = codec.unpackAddress()
-        const asset = codec.unpackID(false)
-        const value = codec.unpackUint64(true)
-        const action = new MintAssetFT(to.toString(), asset.toString(), value)
-        return [action, codec]
-    }
+  marshal(codec: Codec): void {
+    codec.packUint64(this.oldBalance);
+    codec.packUint64(this.newBalance);
+  }
+
+  static unmarshal(codec: Codec): MintAssetFTResult {
+    const oldBalance = codec.unpackUint64(false);
+    const newBalance = codec.unpackUint64(true);
+    return new MintAssetFTResult(oldBalance, newBalance);
+  }
+
+  toJSON(): object {
+    return {
+      oldBalance: this.oldBalance.toString(),
+      newBalance: this.newBalance.toString(),
+    };
+  }
+
+  toString(): string {
+    return JSON.stringify(this.toJSON());
+  }
 }
