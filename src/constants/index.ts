11332372cc6a2be00ce20d3e7121c00c0501018a
developerayo <shodipovi@gmail.com>
2024-10-30 11:01:43 +0100

    remove unneeded files

--- src/constants/index.ts
+++
@@ -1,6 +0,0 @@
-// Copyright (C) 2024, Nuklai. All rights reserved.
-// See the file LICENSE for licensing terms.
-
-export * from '../endpoints'
-// export * from './nuklaivm'
-export * from './nuklaivm_constants'
--- src/services/index.ts
+++
@@ -1,5 +0,0 @@
-// Copyright (C) 2024, Nuklai. All rights reserved.
-// See the file LICENSE for licensing terms.
-
-export * from '../rpcService'
-export * from './websocket'
