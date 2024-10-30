22c315912bf08310cb165c3dca3d3d6ef272e5d9
developerayo <shodipovi@gmail.com>
2024-10-30 11:02:07 +0100

    ...

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 // Copyright (C) 2024, Nuklai. All rights reserved.
 // See the file LICENSE for licensing terms.
 
-export * as actions from "./actions";
-export * as common from "./common";
-export * as consts from "./constants";
-export * as services from "./services";
+// export * as actions from "./actions";
+// export * as common from "./common";
+// export * as consts from "./constants";
+// export * as services from "./services";
 export { NuklaiSDK } from "./sdk";
