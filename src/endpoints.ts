3c38b4cd00b9f738ac1be6a6a085a6c631d1ff5f
Kiran Pachhai <kiran.pachhai@gmail.com>
2024-11-06 20:13:41 -0500

    removing chainid as it's not needed

--- src/endpoints.ts
+++ src/endpoints.ts
@@ -4,7 +4,6 @@
 export const MAINNET_PUBLIC_API_BASE_URL = 'http://127.0.0.1:9650'
 export const VM_NAME = 'nuklaivm'
 export const VM_RPC_PREFIX = 'nuklaiapi'
-export const CHAIN_ID = '2LD7R6qACGZu33upudgAN13EfbDHgYw1bHShqGpbyS6Pt52uEx'
 
 export const getBlockchainEndpoint = (
   baseUrl: string = MAINNET_PUBLIC_API_BASE_URL
