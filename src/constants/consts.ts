26d9140020e13b115b7be2763d3ec625a075f9e8
developerayo <shodipovi@gmail.com>
2024-12-01 11:29:56 +0100

    add missing types

--- src/constants/consts.ts
+++ src/constants/consts.ts
@@ -1,8 +1,17 @@
-// Copyright (C) 2024, Nuklai. All rights reserved.
-// See the file LICENSE for licensing terms.
+import { Id } from '@avalabs/avalanchejs';
+import { Address } from '../utils';
 
-export const ADDRESS_LEN = 20
-export const ID_LEN = 32
-export const UINT8_LEN = 1
+export const ADDRESS_LEN = 20;
+export const ID_LEN = 32;
+export const UINT8_LEN = 1;
+export const BYTE_LEN = 1;
+export const SHORT_LEN = 2;
+export const INT_LEN = 4;
+export const LONG_LEN = 8;
+export const MaxStringLen = 1024;
+export const MaxUint8 = 255;
+export const MillisecondsPerSecond = 1000n;
 
+// Empty instances
 export const EMPTY_ADDRESS = new Address(new Uint8Array(ADDRESS_LEN));
+export const EMPTY_ID = new Id(new Uint8Array(ID_LEN));
