16bd5d7e18678c8015c76f6563a31f3d902283fb
Kiran Pachhai <kiran.pachhai@gmail.com>
2024-12-16 10:49:16 -0500

    Removed console.log

--- src/utils/jsonUtils.ts
+++ src/utils/jsonUtils.ts
@@ -4,7 +4,6 @@
 export function stringifyWithBigInt(value: any): string {
   return JSON.stringify(value, (_, v) => {
     if (typeof v === 'bigint') {
-      console.debug('Handling BigInt:', v) // Add debug log
       return v.toString()
     }
     return v
